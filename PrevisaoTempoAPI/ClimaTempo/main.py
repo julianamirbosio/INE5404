ad4cede2da3ba99c5169775d29182b51d83769db
jubsio <julianamirbosio@gmail.com>
2024-12-23 20:23:03 -0300

    Remoções

--- PrevisaoTempoAPI/ClimaTempo/main.py
+++
@@ -1,9 +0,0 @@
-from janelalogin import JanelaLogin
-
-
-if __name__ == "__main__":
-    print("Iniciando a aplicação")
-    janelalogin = JanelaLogin()
-    janelalogin.configure(fg_color="#ffffff")
-    janelalogin.mainloop()
-    print("Aplicação encerrada")
